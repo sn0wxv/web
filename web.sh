56656e4bf51f0a47dbc4de401af6d8059ff45cf5
sn0wxv <snowaki0x1@gmail.com>
2024-05-16 00:45:50 -0700

    Update web.sh

--- web.sh
+++ web.sh
@@ -11,9 +11,18 @@ echo ""
 echo ""
 echo ""
 
+if [ ! -f ~/wordlists/subdomains.txt ]; then
+    wget -P ~/wordlists http://ffuf.me/wordlist/subdomains.txt
+fi
+
+if [ ! -f ~/wordlists/common.txt ]; then
+    wget -P ~/wordlists http://ffuf.me/wordlist/common.txt
+fi
+
 # Prompt for URL
 echo -e "\e[31mEnter a URL\e[0m"
 read target
+echo ""
 
 # Tidy up URL
 while true; do
@@ -49,8 +58,10 @@ filename="${trimmed_url%%.*}"
 echo -e "Your target is: \e[31m$protocol$target\e[0m"
 sleep 2
 
+# Prompt for option
 echo -e "\e[31mChoose your options:\e[0m \e[34mdirectories\e[0m | \e[35msubdomain\e[0m | \e[33msourcecode\e[0m | \e[36mget\e[0m"
 read -a choices
+echo ""
 
 for choice in ${choices[@]};
 do
@@ -77,13 +88,26 @@ do
     else
       login=""
     fi
-
-    echo "Protocol selected: $protocol"
-    echo "Initializing Fuzz"
-    mode="-s"
-
+    echo "Choose a mode (default is no mode)"
+    echo -e "\e[31m-s\e[0m | \e[35m-v\e[0m | \e[33m-fw\e[0m"
+    read mode
+    if [[ $mode != "-v" ]] && [[ $mode != "-fw" ]] && [[ $mode != "-s" ]]; then
+      mode=""
+    fi
+    echo ""
+    echo -e "\e[31mInitializing Fuzz\e[0m"
+    if [[ $mode = "-s" ]]; then
+      echo ""
+      echo -e "\e[31m     /'___\  /'___\           /'___\  \e[0m"
+      echo -e "\e[31m    /\ \__/ /\ \__/  __  __  /\ \__/  \e[0m"
+      echo -e "\e[31m    \ \ ,__\\ \ ,__\/\ \/\ \ \ \ ,__\ \e[0m"
+      echo -e "\e[31m     \ \ \_/ \ \ \_/\ \ \_\ \ \ \ \_/ \e[0m"
+      echo -e "\e[31m      \ \_\   \ \_\  \ \____/  \ \_\  \e[0m"
+      echo -e "\e[31m       \/_/    \/_/   \/___/    \/_/  \e[0m"
+      echo ""
+    fi
     # Run ffuf for a short duration to gather initial data
-    ffuf -u $protocol$login$target"/FUZZ" -w ~/wordlists/common.txt -mc 100-299,500-599 | tee ~/Desktop/"$filename directories.txt"
+    ffuf -u $protocol$login$target"/FUZZ" $mode -w ~/wordlists/common.txt -mc 100-299,500-599 | tee ~/Desktop/"$filename directories.txt"
 
     ;;
   subdomains)
@@ -99,11 +123,29 @@ do
       login=""
     fi
 
-    echo "Initializing Fuzz"
+    echo "Choose a mode (default is no mode)"
+    echo -e "\e[31m-s\e[0m | \e[35m-v\e[0m | \e[33m-fw\e[0m"
+    read mode
+    if [[ $mode != "-v" ]] && [[ $mode != "-fw" ]] && [[ $mode != "-s" ]]; then
+      mode=""
+    fi
+    echo ""
+    echo -e "\e[31mInitializing Fuzz\e[0m"
+    if [[ $mode = "-s" ]]; then
+      echo ""
+      echo -e "\e[31m     /'___\  /'___\           /'___\  \e[0m"
+      echo -e "\e[31m    /\ \__/ /\ \__/  __  __  /\ \__/  \e[0m"
+      echo -e "\e[31m    \ \ ,__\\ \ ,__\/\ \/\ \ \ \ ,__\ \e[0m"
+      echo -e "\e[31m     \ \ \_/ \ \ \_/\ \ \_\ \ \ \ \_/ \e[0m"
+      echo -e "\e[31m      \ \_\   \ \_\  \ \____/  \ \_\  \e[0m"
+      echo -e "\e[31m       \/_/    \/_/   \/___/    \/_/  \e[0m"
+      echo ""
+    fi
+
     target="${target#www.}"
 
     # Run ffuf
-    ffuf -u $protocol$login"FUZZ."$target -s -w ~/wordlists/subdomains.txt -mc 100-299,500-599 | tee ~/Desktop/"$filename subdomains.txt"
+    ffuf -u $protocol$login"FUZZ."$target $mode -w ~/wordlists/subdomains.txt -mc 100-299,500-599 | tee ~/Desktop/"$filename subdomains.txt"
     target="www."$target
     ;;
   source)
@@ -124,4 +166,3 @@ do
     ;;
   esac
 done
-
